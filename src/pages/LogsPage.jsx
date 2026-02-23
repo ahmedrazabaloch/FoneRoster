7fd92a70d6ac314a51f51f7f1c24e2580a03b746
Ahmed raza <ahmed.raza1054@gmail.com>
2026-02-23 07:11:58 +0500

    Add LogsPage component

--- src/pages/LogsPage.jsx
+++ src/pages/LogsPage.jsx
@@ -1,4 +1,4 @@
 import React from 'react';
-import { LogViewer } from '../features/logs/LogViewer';
+import { LogViewer } from '../features/logs/LogViewer.jsx';
 
 export const LogsPage = () => <LogViewer />;
